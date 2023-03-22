a2e37691286d245e3988ea61a86d0192d3259d8e
Ansh Bhardwaj <n01552508@humber.ca>
2023-03-22 13:32:03 -0400

    module-09

--- module-08/cb-hell.js
+++ module-08/cb-hell.js
@@ -4,53 +4,39 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
-}
+const timeout = async (ms) =>{
+      setTimeout( () => { }, ms);
+  };
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
+  const generateRandomNumber = () =>{
+    return  Math.floor(Math.random() * 40);
+  }
 
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
+  const generateData = async () => {
+    await timeout(1000);
+    await processData(Array.from({ length: 20 }, generateRandomNumber));
+  }
 
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
-}
+  const convertToFeet = async (meters) => {
+    const feet = meters * 3.2808;
+    await timeout(3500);
+    await logResult(meters,feet)
+  }
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
-
-function logResult(meters, feet) {
-  console.log(`Converted ${meters}m to ${feet}ft`);
-}
-
-function main() {
-  console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
+  const processData = async (data) => {
+    data.map(async (value)=> {
+      await convertToFeet(value);
     });
-  });
-  console.log("Finish");
-}
+  }
 
-main();
+  const logResult = async (meters, feet) => {
+    console.log(`Converted ${meters}m to ${feet}ft`);
+  }
+
+  const main = async () => {
+    console.log("Start");
+    await generateData();
+    console.log("Finish");
+  }
+
+  main();
